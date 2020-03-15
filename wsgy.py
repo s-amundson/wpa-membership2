de45c4ce1890d52623d63027ae2cc9a39ddd7307
Sam <sam.amundson@gmail.com>
2020-03-15 11:19:58 -0700

    updated wsgy.py

--- wsgy.py
+++ wsgy.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 import sys
-sys.path.insert(0,"/var/www/wpa-membership2/")
+import os
+
+project_directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0,project_directory)
+sys.path.insert(0,os.path.join(project_directory, 'src'))
 from application import app as application

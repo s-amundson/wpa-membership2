e6f4eea8f96a0a62cb73a13a4c29df3a5b2b290a
Sam <sam.amundson@gmail.com>
2020-04-10 08:55:11 -0700

    accidentally deleted line in wsgy.py

--- wsgy.py
+++ wsgy.py
@@ -2,7 +2,8 @@
 import sys
 import os
 
-# For apache2 use
+# To be used with apache2 and mod_wsgi
 project_directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0,project_directory)
 sys.path.insert(0,os.path.join(project_directory, 'src'))
+from application import app as application
